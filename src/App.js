796aebb77f16de6b15300f6db5c5dfa0b353a543
Jock Dalby <jockdalby86@gmail.com>
2018-10-14 06:54:04 +1300

    create Aggregate component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div className="aggregate">
+        <h2 style={{ color: 'blue' }}>Number Text</h2>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Title</h1>
+        <Aggregate></Aggregate>
       </div>
     );
   }
