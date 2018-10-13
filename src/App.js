ff2d75afdf4bc412aacebdf7493fc5b9023717dc
Jock Dalby <jockdalby86@gmail.com>
2018-10-14 07:04:33 +1300

    create Filter and PlaylistItem components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const defaultTextColor = '#fff';
+
 class Aggregate extends Component {
   render() {
     return (
-      <div className="aggregate">
-        <h2 style={{ color: 'blue' }}>Number Text</h2>
+      <div style={{ width: '40%', display: 'inline-block' }}>
+        <h2 style={{ color: defaultTextColor }}>Number Text</h2>
       </div>
     )
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class PlaylistItem extends Component {
+  render() {
+    return (
+      <div style={{ color: defaultTextColor }}>
+        <img />
+        <h3>Playlist name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
   }
 }
 
@@ -15,8 +44,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{ color: defaultTextColor }}>Title</h1>
         <Aggregate></Aggregate>
+        <Aggregate></Aggregate>
+        <Filter/>
+        <PlaylistItem/>
       </div>
     );
   }
